84a942829e7cb3cffe91ec20b8e711a77718631a
Sebastian Bauer <admin@e-back.net>
2016-09-28 10:00:14 +0200

    add thot_decategorize to utils

---
+++ thot_utils/bin/thot_decategorize.py
@@ -0,0 +1,55 @@
+# Author: Daniel Ortiz Mart\'inez
+# *- python -*
+
+# import modules
+import argparse
+import io
+import itertools
+
+from thot_utils.libs import thot_preproc
+
+argparser = argparse.ArgumentParser(description=__doc__)
+argparser.add_argument(
+    '-s',
+    '--source-file',
+    type=str,
+    help='File with source text',
+    required=True,
+)
+argparser.add_argument(
+    '-t',
+    '--target-file',
+    type=str,
+    help='File with target text',
+    required=True,
+)
+
+argparser.add_argument(
+    '-i',
+    '--hypothesis-file',
+    type=int,
+    help='Minimum sentence length (1 by default)',
+    default=1,
+)
+
+
+##################################################
+def main():
+    cli_args = argparser.parse_args()
+
+    sfile = io.open(cli_args.source_file, 'r', encoding='utf-8')
+    tfile = io.open(cli_args.target_file, 'r', encoding='utf-8')
+    ifile = io.open(cli_args.hypothesis_file, 'r', encoding='utf-8')
+
+    for sline, tline, iline in itertools.izip(sfile, tfile, ifile):
+        # Read source, target and hypothesis information
+        sline = sline.strip('\n')
+        tline = tline.strip('\n')
+        iline = iline.strip('\n')
+
+        decategorized_line = thot_preproc.decategorize(sline, tline, iline)
+        print decategorized_line.encode('utf-8')
+
+
+if __name__ == '__main__':
+    main()
